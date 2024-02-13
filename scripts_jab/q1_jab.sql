b8337ed074f98adbd96f3820836607a0d801deea
jaredjab <jaredbakerbox@gmail.com>
2024-02-13 14:58:12 -0600

    Added data dictionary and script files

---
+++ scripts_jab/q1_jab.sql
@@ -0,0 +1,5 @@
+-- Q1 -- Lahman Baseball Project -- The Hackstreet Boys -- Jared Baker
+/*
+What range of years for baseball games played does the provided database cover?
+*/
+
---
+++ scripts_jab/q2_jab.sql
@@ -0,0 +1,5 @@
+-- Q2 -- Lahman Baseball Project -- The Hackstreet Boys -- Jared Baker
+/*
+Find the name and height of the shortest player in the database. How many games did he play in? What is the name of the team for which he played?
+*/
+
